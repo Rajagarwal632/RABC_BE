06bf70f67711ae5c4be202b0c75bf9e1106de480
Rajagarwal632 <raj390504@gmail.com>
2025-12-18 12:22:35 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+const {userroute} = require("./route/user")
+// const {todoroute} = require("./route/todo")
+const express = require ("express")
+const app = express()
+
+app.use(express.json())
+app.use("/user" , userroute)
+// app.use("/todo" , todoroute)
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(process.env.PORT)
+    console.log(`Listening to port - ${process.env.PORT}\nconnected to mongo!!`)
+}
+main()
