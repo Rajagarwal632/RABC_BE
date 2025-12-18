5cec4b108a2df0e20f82e3124652ff0fe3f2704a
Rajagarwal632 <raj390504@gmail.com>
2025-12-18 13:21:02 +0530

    error handling updated

--- middleware/userauth.js
+++ middleware/userauth.js
@@ -3,15 +3,22 @@ const JWT_USER = process.env.JWT_USER
 
 function userauth(req,res,next){
     const token = req.headers.token
-    const decodeddata = jwt.verify(token,JWT_USER)
-    if(decodeddata){
-        req.userid = decodeddata
-        next()
-    }else{
-        res.json({
-            msg : "INCORRECT CREDENTIALS"
+    if(!token){
+        return res.status(401).json({
+            msg : "TOKEN MISSING"
         })
     }
+    try{
+        const decodeddata = jwt.verify(token,JWT_USER)
+        req.userid = decodeddata
+        next()
+        
+    }
+    catch(error){
+        res.status(401).json({
+            msg : "INCORRECT CREDENTIALS"
+        })
+    }   
 }
 module.exports = {
     userauth
