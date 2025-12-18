2c87b353ddf127ae5397627af852c7fda46f7bed
Rajagarwal632 <raj390504@gmail.com>
2025-12-18 15:01:47 +0530

    /admin/assign_role defined

--- route/admin.js
+++ route/admin.js
@@ -13,6 +13,35 @@ adminroute.get("/" , userauth , roleauth(["admin"]),function(req,res){
     })
 })
 
+adminroute.patch("/assign-role/:id",userauth,roleauth(["admin"]),async function(req,res){
+    const role = req.body.role
+    const userid = req.params.id
+
+    const validroles = ["admin" , "user" , "moderator"]
+    if(!validroles.includes(role)){
+        return res.status(400).json({
+            msg : "INVALID ROLE"
+        })
+    }
+    const user = await usermodel.findOneAndUpdate({
+        _id : userid
+    },{
+        role : role,
+    },{
+        new : true
+    })
+    if(!user){
+        return res.status(404).json({
+            msg : "USER NOT FOUND"
+        })
+    }
+    res.json({
+        msg : "ROLE UPDATED",
+        userid : user._id,
+        newrole : role
+    })
+})
+
 module.exports = {
     adminroute
 }
